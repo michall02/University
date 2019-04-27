1e52552ff9f708afe908bc020d88be2ce4830b0a
Michał Kasprzak <michalxkasprzak@gmail.com>
2019-04-27 14:59:01 +0200

    add remove student layout and functionality

--- university-data/src/main/java/pl/home/utils/NotificationMessages.java
+++ university-data/src/main/java/pl/home/utils/NotificationMessages.java
@@ -9,7 +9,9 @@ public enum NotificationMessages {
     STUDENT_SAVE_VALIDATION_ERROR_TITLE("Error"),
     STUDENT_SAVE_VALIDATION_ERROR_DESCRIPTION("All fields must be filled"),
     STUDENT_SAVE_VALIDATION_ACCEPTED_TITLE("Saved"),
-    STUDENT_SAVE_VALIDATION_ACCEPTED_DESCRIPTION("Student has been added");
+    STUDENT_SAVE_VALIDATION_ACCEPTED_DESCRIPTION("Student has been added"),
+    STUDENT_REMOVE_SUCCESS_TITLE("Removed"),
+    STUDENT_REMOVE_SUCCESS_DESCRIPTION("Student(s) successfully removed");
 
     private String value;
 
---
+++ university-web/src/main/java/pl/home/ui/students/RemoveStudentLayoutFactory.java
@@ -0,0 +1,89 @@
+package pl.home.ui.students;
+
+import com.vaadin.data.provider.ListDataProvider;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.components.grid.MultiSelectionModel;
+import com.vaadin.ui.themes.ValoTheme;
+import pl.home.models.Student;
+import pl.home.services.RemoveStudentService;
+import pl.home.services.ShowAllStudentsService;
+import pl.home.ui.commons.UniversityMainUI;
+
+import java.util.List;
+
+import static pl.home.utils.NotificationMessages.STUDENT_REMOVE_SUCCESS_DESCRIPTION;
+import static pl.home.utils.NotificationMessages.STUDENT_REMOVE_SUCCESS_TITLE;
+
+@SpringView(name = RemoveStudentLayoutFactory.NAME, ui = UniversityMainUI.class)
+public class RemoveStudentLayoutFactory extends VerticalLayout implements View, Button.ClickListener {
+    public static final String NAME = "removestudent";
+
+    private Grid<Student> removeStudentTable;
+    private Button removeStudentBtn;
+    private List<Student> students;
+    private ListDataProvider<Student> listProvider;
+
+    private final ShowAllStudentsService showAllStudentsService;
+    private final RemoveStudentService removeStudentService;
+
+    public RemoveStudentLayoutFactory(ShowAllStudentsService showAllStudentsService, RemoveStudentService removeStudentService) {
+        this.showAllStudentsService = showAllStudentsService;
+        this.removeStudentService = removeStudentService;
+    }
+
+    public void addLayout(){
+        setMargin(true);
+        removeStudentBtn = new Button("Remove");
+        removeStudentBtn.setStyleName(ValoTheme.BUTTON_DANGER);
+        listProvider = new ListDataProvider<>(students);
+
+        removeStudentTable = new Grid<>(Student.class);
+        removeStudentTable.setDataProvider(listProvider);
+        removeStudentTable.setColumns("firstName", "lastName", "age", "gender");
+        removeStudentTable.setSelectionMode(Grid.SelectionMode.MULTI);
+
+        removeStudentBtn.addClickListener(this);
+        addComponent(removeStudentTable);
+        addComponent(removeStudentBtn);
+
+    }
+
+    private void loadStudents() {
+        students = showAllStudentsService.getAllStudent();
+    }
+
+    @Override
+    public void buttonClick(Button.ClickEvent clickEvent) {
+        MultiSelectionModel selectionModel = (MultiSelectionModel) removeStudentTable.getSelectionModel();
+
+        for (Object selectedItem : selectionModel.getSelectedItems()) {
+            Student student = (Student) selectedItem;
+            listProvider.getItems().remove(student);
+            removeStudentService.removeStudent(student);
+            removeStudentTable.getDataProvider().refreshAll();
+        }
+        Notification.show(STUDENT_REMOVE_SUCCESS_TITLE.getValue(),
+                STUDENT_REMOVE_SUCCESS_DESCRIPTION.getValue(),
+                Notification.Type.WARNING_MESSAGE);
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+
+        if(removeStudentTable  != null){
+            return;
+        }
+
+        loadStudents();
+        addLayout();
+    }
+
+
+
+}
